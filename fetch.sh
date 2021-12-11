17fb6c4bef12477aa03c84a1964d2f1d10811afd
Mark Schmidt <mark.schmidt@hey.com>
2021-12-11 07:45:18 -0600

    Fix fetch script ?

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# AOC_TOKEN=$(cat AOC_TOKEN)
-
 if [ -z "$1" ]; then
   echo "Provide a day number."
   echo "usage: $0 DAY"
@@ -16,7 +14,7 @@ fi
 URL="https://adventofcode.com/2021/day/$1/input"
 cargo new day$1
 cd day$1
-curl $URL --cookie "session=$AOC_SESSION" -s > input.txt
+curl $URL --cookie $AOC_SESSION > input.txt
 touch sample.txt
 cp ../template.rs src/main.rs
 echo "itertools = \"0.8.0\"" >> Cargo.toml
